2f4b69a7f61c91aa34245bda9023f98c9825471e
Satish Rapol <52364368+satishrapol@users.noreply.github.com>
2020-07-25 01:07:13 +0530

    Update satish_opf_casadi.py

--- satish_opf_casadi.py
+++ satish_opf_casadi.py
@@ -12,7 +12,7 @@ G = np.array([ \
     ])
 # P_load = np.array([3.00, 3.00, 3.00,4.00, 0.00])
 P_load = np.array([3.00, 3.00, 3.00,4.00, 0.00])
-alpha = 5
+alpha = 6
 # alpha_01 = 0.1
 beta  = 14
 gamma = 1
